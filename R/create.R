37957ee46c32ce82542a5dca886af654089c32d1
Jamie Owen <jamie@jumpingrivers.com>
2022-04-13 00:01:25 +0100

    feat: Create location for routes (#11)
    * feat: Create location for routes
    
    * build: Version and news

--- R/create.R
+++ R/create.R
@@ -1,4 +1,5 @@
 create_from_template = function(..., target_name) {
+  check_pipework()
   in_file = template_file(...)
   if (missing(target_name)) {
     target_name = basename(in_file)
@@ -6,4 +7,14 @@ create_from_template = function(..., target_name) {
   out_file = file.path("R", target_name)
   fs::file_copy(in_file, out_file)
   cli::cli_alert_success("Created file {.file {out_file}}")
+}
+
+create_routes_dir = function() {
+  check_pipework()
+  target_dir = "inst/extdata/api/routes"
+  created = dir.create(target_dir, recursive = TRUE)
+  if (created) {
+    cli::cli_alert_success("Create directory {.file {target_dir}}")
+  }
+  invisible(created)
 }
--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -1,9 +1,23 @@
 test_that("create from template", {
   ppath = with_temp_package()
+  # fails without pipework lock file
+  expect_error(create_from_template("R", "_generate_api.R"))
+
+  create_lock_file()
   expect_message(create_from_template("R", "_generate_api.R"))
   expect_true(file.exists(file.path(ppath, "R", "_generate_api.R")))
 
   # using non default target name
   create_from_template("R", "_generate_api.R", target_name = "test_name.R")
   expect_true(file.exists(file.path(ppath, "R", "test_name.R")))
+})
+
+test_that("create route directory", {
+  ppath = with_temp_package()
+  # fails without pipework lock file
+  expect_error(create_routes_dir())
+
+  create_lock_file()
+  expect_message(create_routes_dir())
+  expect_true(dir.exists("./inst/extdata/api/routes"))
 })
