95a0d0fdb494fddb55d151d79e81699b8a80c57f
Jamie <jamie@jumpingrivers.com>
2022-04-13 00:29:15 +0100

    feat: Start adding lay_pipework

---
+++ R/lay.R
@@ -0,0 +1,21 @@
+lay_pipework = function() {
+  check_package()
+  if (is_pipework()) {
+    stop("This looks to already be a pipework project", call. = FALSE)
+  }
+  cli::cli_h1("Laying pipe")
+  cli::cli_h2("Creating lock file")
+  create_lock_file()
+
+  cli::cli_h2("Generating code")
+  create_from_template("R", "_generate_api.R")
+
+  cli::cli_h3("Adding dependencies for generated code")
+  add_imports(c(
+    "utils", "stringr", "plumber",
+    "purrr"
+  ))
+
+  cli::cli_h2("Creating location for route definitions")
+  create_routes_dir()
+}
