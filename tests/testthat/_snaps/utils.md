581ed788738ed8f62530572d302a3ae1cba73ab2
Jamie <jamie@jumpingrivers.com>
2022-07-21 21:01:14 +0100

    test: drop snapshots for now

--- tests/testthat/_snaps/utils.md
+++
@@ -1,7 +0,0 @@
-# build ignore pipework
-
-    Code
-      build_ignore_pipework()
-    Message <rlang_message>
-      v Adding '^\\.pipework$' to '.Rbuildignore'
-
--- tests/testthat/test-add.R
+++ tests/testthat/test-add.R
@@ -2,14 +2,15 @@ describe(
   "add_logging()", {
     it("will error when package is not pipework project", {
       ppath = with_temp_package()
-      expect_snapshot(error = TRUE, {
+      expect_error(#error = TRUE, {
         add_logging()
-      })
+      #}
+      )
     })
     it("successfully adds logging functions to an existing pipework project", {
       ppath = with_temp_package()
       create_lock_file()
-      expect_snapshot({
+      expect_message({
         add_logging()
       })
       expect_true(file.exists(file.path(ppath, "R", "logger_.R")))
@@ -21,14 +22,15 @@ describe(
   "add_entrypoint()", {
     it("will error when package is not a pipework project", {
       ppath = with_temp_package()
-      expect_snapshot(error = TRUE, {
+      expect_error(#error = TRUE, {
         add_entrypoint()
-      })
+      #}
+      )
     })
     it("successfully adds entrypoint to root of exsiting pipework project", {
       ppath = with_temp_package()
       create_lock_file()
-      expect_snapshot({
+      expect_message({
         add_entrypoint()
       })
       expect_true(file.exists(file.path(ppath, "entrypoint.R")))
